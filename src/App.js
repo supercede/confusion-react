753347009e8c3dcbc954519d9e4c19daceebfd2c
Sijuade Ajagunna <mahyor.sam@gmail.com>
2020-02-04 04:55:05 +0100

    add menu component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/Menu.component";
 import "./App.css";
 
 const App = () => (
-  <div className='App'>
+  <div>
     <Navbar dark color='primary'>
       <div className='container'>
         <NavbarBrand href=''>Ristorante Con Fusion</NavbarBrand>
       </div>
     </Navbar>
+    <Menu />
   </div>
 );
 

dc3d1ab1b409ae9f92f0f1940e1d56f290076c8d
Sijuade Ajagunna <mahyor.sam@gmail.com>
2020-02-17 04:21:28 +0100

    add controlled contact form

--- src/components/homeComponent.js
+++ src/components/homeComponent.js
@@ -1,36 +1,44 @@
 import React from 'react';
-import { Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText } from 'reactstrap';
+import {
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText
+} from 'reactstrap';
 
-function RenderFeaturedCard({item}) {
-    return(
-        <Card>
-            <CardImg src={item.image} alt={item.name} />
-            <CardBody>
-            <CardTitle>{item.name}</CardTitle>
-            {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
-            <CardText>{item.description}</CardText>
-            </CardBody>
-        </Card>
-    );
-
+function RenderFeaturedCard({ item }) {
+  return (
+    <Card>
+      <CardImg src={item.image} alt={item.name} />
+      <CardBody>
+        <CardTitle>{item.name}</CardTitle>
+        {item.designation ? (
+          <CardSubtitle>{item.designation}</CardSubtitle>
+        ) : null}
+        <CardText>{item.description}</CardText>
+      </CardBody>
+    </Card>
+  );
 }
 
 function Home({ dish, promotion, leader }) {
-    return (
-        <div className='container'>
-            <div className='row align-items-start'>
-                <div className='col-12 col-md m-1'>
-                    <RenderFeaturedCard item={dish} />
-                </div>
-                <div className='col-12 col-md m-1'>
-                    <RenderFeaturedCard item={promotion} />
-                </div>
-                <div className='col-12 col-md m-1'>
-                    <RenderFeaturedCard item={leader} />
-                </div>
-            </div>
+  return (
+    <div className='container'>
+      <div className='row align-items-start'>
+        <div className='col-12 col-md m-1'>
+          <RenderFeaturedCard item={dish} />
         </div>
-    )
+        <div className='col-12 col-md m-1'>
+          <RenderFeaturedCard item={promotion} />
+        </div>
+        <div className='col-12 col-md m-1'>
+          <RenderFeaturedCard item={leader} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
